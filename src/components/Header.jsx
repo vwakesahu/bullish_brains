5393eb1fa97d20a009f4e062e0ea10920bd13d6f
VivekPC <viveksahu1762@gmail.com>
2023-03-25 20:35:19 +0530

    NavBar Process Initiated

---
+++ src/components/Header.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../img/logo.png'
+import Avatar from '../img/avatar.png';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { FaShoppingCart } from 'react-icons/fa';
+
+import { TbLogout } from 'react-icons/tb';
+
+
+
+import { motion } from "framer-motion";
+
+
+const Header = () => {
+
+    const [isMenu, setIsMenu] = useState(false);
+
+
+
+    return (
+        <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
+            {/* Desktop & Tablet*/}
+            <div className="hidden md:flex w-full h-full items-center justify-between">
+            <Link to={""} className='flex items-center gap-2'> 
+          <img src={Logo} className='w-[70px] -mt-2 object-cover' alt="logo" />
+          <p className='text-headingColor text-3xl font-bold relative'>Bullish Brains<p className='text-[10px] -mt-[15px]'>-By Initiators</p></p>
+        </Link>
+
+        <div className="flex items-center gap-8">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-24 "
+          >
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+          <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Explore</li>
+          <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+          <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+        
+          </motion.ul>
+
+          <div
+            className="relative flex items-center justify-center"
+          >
+            
+          </div>
+
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={Avatar}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt="userprofile"
+            />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                >
+                  Logout
+                </p>
+              </motion.div>
+            )}
+          </div>
+        </div>
+      </div>
+
+
+            {/*For Mobile*/}
+            <div className='flex items-center justify-between md:hidden w-full h-full'>
+
+                <div className='relative flex items-center justify-center -left-6' >
+                    <FaShoppingCart className='text-textColor text-3xl ml-8 cursor-pointer' />
+
+                    <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                        <p className='text-xs text-white font-semibold'>{89}</p>
+                    </div>
+
+                </div>
+                <Link to={"/"} className='flex items-center gap-0'>
+                    <img src={Logo} className='w-12 object-cover' alt="logo" />
+                    <p className='text-headingColor text-2xl font-bold'>TEEN</p>
+                </Link>
+
+
+                <div className='relative'>
+                    <motion.img
+                        whileTap={{ scale: 0.6 }}
+                        src={Avatar}
+                        className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
+                        alt="user profile"
+                    />
+                    {
+                        isMenu && (
+                            <motion.div
+                                initial={{ opacity: 0, scale: 0.6 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.6 }}
+                                className='w-40 bg-green-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+
+                                <Link to={'/createItem'}>
+                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <AiOutlinePlus /></p>
+                                </Link>
+
+
+                                <ul
+                                    className='flex flex-col'>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2
+              onClick={() => setIsMenu (false)}'>Home</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2
+              onClick={() => setIsMenu (false)}'>Menu</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2
+              onClick={() => setIsMenu (false)}'>About Us</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2
+              onClick={() => setIsMenu (false)}'>Service</li>
+                                </ul>
+
+                                <p className='m-2 p-2 rounded-md shadow:md flex items-center justify-center bg-gray-300 gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base'>
+                                    Log Out<TbLogout />
+                                </p>
+                            </motion.div>
+                        )
+                    }
+                </div>
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
