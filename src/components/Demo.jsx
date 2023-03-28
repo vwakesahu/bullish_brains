ac2e540b2103c38f79625233063e4ae9427b8788
VivekPC <viveksahu1762@gmail.com>
2023-03-28 23:00:17 +0530

    Login page ui, signup page ui, footer

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,17 +4,22 @@ import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import 'chartjs-adapter-date-fns';
 import { Box, Text } from '@chakra-ui/react';
-import Loader from './Loader';
+import Loader from './Loader'
+const Demo = () => {
+  let stockname='AAPL'
 
-const StockChart = () => {
+  
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      const apikey='YYGFPGKNWVMLOYMK'
+      
+      let apiurl=`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=30sec&apikey=${apikey}`
       const result = await axios.get(
-        'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=RELIANCE.BSE&outputsize=full&apikey=YYGFPGKNWVMLOYMK'
+        apiurl
       );
-      setData(result.data['Time Series (Daily)']);
+      setData(result.data['Time Series (30sec)']);
     };
     fetchData();
   }, []);
@@ -121,6 +126,7 @@ const StockChart = () => {
 
 
 
+
   return (
     <div className="flex flex-col items-center justify-center space-y-4 h-screen">
       {!data ? (
@@ -133,15 +139,13 @@ const StockChart = () => {
           Reliance Stock Price Chart
         </Text>
         <Text fontSize="sm" color="#333" textAlign="center">
-          The chart displays the daily open price of Reliance Industries Ltd. (RELIANCE.BSE) from the last 18 years.
+          The chart displays the daily open price of Reliance Industries Ltd. {stockname} from the last 18 years.
         </Text>
         <Line data={chartData} options={options} className="w-full" />
       </Box>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default StockChart;
-
-
+export default Demo
---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { FaTwitter, FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { AiFillPhone, AiFillMail } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap justify-between">
+        <div className="w-full md:w-1/4 mb-4 md:mb-0">
+          <Link to="/" className="text-2xl font-bold mb-2 block">
+            Bullish Brains 
+          </Link>
+          <p className="text-gray-400">Just a demo of stock page</p>
+        </div>
+        <div className="w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
+          <ul className="list-unstyled">
+            <li>
+              <Link to="/" className="hover:text-gray-400 block mb-2">Home</Link>
+            </li>
+            <li>
+              <Link to="/stocks" className="hover:text-gray-400 block mb-2">Stocks</Link>
+            </li>
+            <li>
+              <Link to="/news" className="hover:text-gray-400 block mb-2">News</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
+          <p className="flex items-center">
+            <AiFillPhone className="mr-2" />
+            +91 1234567829
+          </p>
+          <p className="flex items-center">
+            <AiFillMail className="mr-2" />
+            initiator@bullishbrains.com
+          </p>
+        </div>
+        <div className="w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
+          <div className="flex">
+          <a href="https://github.com/vwakesahu/bullish_brains" className=" mr-4 hover:text-gray-400">
+              <FaGithub />
+            </a>
+            <a href="#" className="mr-4 hover:text-gray-400">
+              <FaTwitter />
+            </a>
+            <a href="#" className="mr-4 hover:text-gray-400">
+              <FaFacebook />
+            </a>
+            <a href="#" className="mr-4 hover:text-gray-400">
+              <FaLinkedin />
+            </a>
+            
+          </div>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8 text-center text-gray-400">
+        <p>&copy; {new Date().getFullYear()} StockMarket. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
